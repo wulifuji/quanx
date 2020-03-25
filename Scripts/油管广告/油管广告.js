e93b7cd3f0d8297271d88ef0090edb9e9b16514f
这与我单手开拖拉机有什么关系 <61496038+wulifuji@users.noreply.github.com>
2020-03-25 12:26:36 +0800

    Update 油管广告.js

--- Scripts/油管广告/油管广告.js
+++ Scripts/油管广告/油管广告.js
@@ -1,6 +1,7 @@
 // by choler  
 //油管广告.js
-
+//[MITM]  hostname = *.googlevideo.com
+//[rewrite_local]  http-request ^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) url script-response-body 油管广告.js
 var data = {
   body: "{}",
   headers: {
